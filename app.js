8d63a30689da7f47f788c3681afd3af8932da296
DeathForRussia <mestcomua@gmail.com>
2023-05-13 17:01:42 +0300

    work in Araks

--- app.js
+++ app.js
@@ -1,67 +1,67 @@
 // Task: Converter WorkDays-CalendarDays
 {
-const bid = 80;
-let deadLine = 11;
-let workHours = 5;
-let workDays = 5;
-let weekends = 7 - workDays;
-let timeForTask = 40;
-let workDaysCount = timeForTask / workHours
-let mySalary = bid * timeForTask;
-let isOpportunity = false;
+	const bid = 80;
+	let deadLine = 11;
+	let workHours = 5;
+	let workDays = 5;
+	let weekends = 7 - workDays;
+	let timeForTask = 40;
+	let workDaysCount = timeForTask / workHours
+	let mySalary = bid * timeForTask;
+	let isOpportunity = false;
 
-let a = timeForTask / (workDays * workHours)
+	let a = timeForTask / (workDays * workHours)
 
-let weeksCount = Math.trunc(a);
+	let weeksCount = Math.trunc(a);
 
-let calendarDaysCount = (weeksCount * workDays) + (weeksCount * weekends) + (workDaysCount % 5);
+	let calendarDaysCount = (weeksCount * workDays) + (weeksCount * weekends) + (workDaysCount % 5);
 
-if(calendarDaysCount <= deadLine){
-	isOpportunity = true;
-	console.log('Я зроблю цю роботу за ' + mySalary + ' долларів');
-}
+	if (calendarDaysCount <= deadLine) {
+		isOpportunity = true;
+		console.log('Я зроблю цю роботу за ' + mySalary + ' долларів');
+	}
 
-console.log(calendarDaysCount);
-console.log('Чи встигну я? - ' + isOpportunity);
+	console.log(calendarDaysCount);
+	console.log('Чи встигну я? - ' + isOpportunity);
 
 }
 
 // Chapter 4 Lesson 17-19
 {
-let age = 18;
+	let age = 18;
 
-console.log(age + 5);
-console.log(age - 3);
-console.log(age / 3);
-console.log(age * 4);
-console.log(String(4) + 7);
-console.log(Boolean(''));
-console.log(typeof NaN);
+	console.log(age + 5);
+	console.log(age - 3);
+	console.log(age / 3);
+	console.log(age * 4);
+	console.log(String(4) + 7);
+	console.log(Boolean(''));
+	console.log(typeof NaN);
 
-a = 2 + '10';
-console.log(a - 10);
-let g = null;
-console.log(g);
-console.log(Boolean(0));
-console.log(Boolean(''));
-console.log(Boolean(undefined));
-console.log(Boolean(null));
-console.log(Boolean(Number('sx')));
+	a = 2 + '10';
+	console.log(a - 10);
+	let g = null;
+	console.log(g);
+	console.log(Boolean(0));
+	console.log(Boolean(''));
+	console.log(Boolean(undefined));
+	console.log(Boolean(null));
+	console.log(Boolean(Number('sx')));
 }
 
 // Chapter 5 Lesson 20
 {
-const money = 45;
-const canBuy = money > 50;
+	const money = 45;
+	const canBuy = money > 50;
 
-if(canBuy){
-	console.log('You can buy it');
-}else if(money > 5){
-	console.log('You buy mini product');
-}
-else{
-	console.log('Can`t buy');
-}
+	if (canBuy) {
+		console.log('You can buy it');
+	} else if (money > 5) {
+		console.log('You buy mini product');
+	}
+	else {
+		console.log('Can`t buy');
+	}
 }
 
 // Chapter 5 Lesson 21 Task Складний процент накопичень
@@ -70,33 +70,33 @@ else{
 	const persent = 0.07;
 	let numberMonth = 24;
 
-	let result = capital * Math.pow(1 + (persent / numberMonth),(numberMonth * 2));
+	let result = capital * Math.pow(1 + (persent / numberMonth), (numberMonth * 2));
 	console.log(result);
 	console.log(result + capital);
 
 
 	function calculateCompoundInterest(PV, r, n, t) {
-  let FV = PV * Math.pow(1 + (r/n), n*t);
-  return FV.toFixed(2); // округляем до двух знаков после запятой
-}
+		let FV = PV * Math.pow(1 + (r / n), n * t);
+		return FV.toFixed(2); // округляем до двух знаков после запятой
+	}
 
-// Пример использования функции
-let PV = 12000; // текущая стоимость инвестиции
-let r = 0.07; // годовая процентная ставка (6%)
-let n = 24; // проценты начисляются ежемесячно
-let t = 2; // срок инвестирования в годах
-let FV = calculateCompoundInterest(PV, r, n, t); // вычисляем будущую стоимость инвестиции
-console.log("Будущая стоимость инвестиции: $" + FV);
+	// Пример использования функции
+	let PV = 12000; // текущая стоимость инвестиции
+	let r = 0.07; // годовая процентная ставка (6%)
+	let n = 24; // проценты начисляются ежемесячно
+	let t = 2; // срок инвестирования в годах
+	let FV = calculateCompoundInterest(PV, r, n, t); // вычисляем будущую стоимость инвестиции
+	console.log("Будущая стоимость инвестиции: $" + FV);
 
 }
 
 // Chapter 5 Lesson 22 Оператори рівності
 {
 	const secretNumber = 7;
-	if(secretNumber === 7){
+	if (secretNumber === 7) {
 		console.log('Вгадав точно');
 	}
-	if(secretNumber == 7){
+	if (secretNumber == 7) {
 		console.log('Вгадав неточно');
 	}
 
@@ -109,9 +109,9 @@ console.log("Будущая стоимость инвестиции: $" + FV);
 // Chapter 5 Lesson 23 Switch
 {
 	const role = 'manager';
-	if (role === 'manager'){
+	if (role === 'manager') {
 		console.log('Менеджер');
-	}else if (role === 'admin'){
+	} else if (role === 'admin') {
 		console.log('Адмін');
 	}
 
@@ -126,12 +126,12 @@ console.log("Будущая стоимость инвестиции: $" + FV);
 			console.log('СЕО');
 			break;
 		default:
-		console.log('Я тебе не знаю');
+			console.log('Я тебе не знаю');
 
 	}
 
 	switch (role) {
-		case 'manager':			
+		case 'manager':
 		case 'admin':
 			console.log('Не керівник');
 			break;
@@ -139,7 +139,7 @@ console.log("Будущая стоимость инвестиции: $" + FV);
 			console.log('Керівник');
 			break;
 		default:
-		console.log('Я тебе не знаю');
+			console.log('Я тебе не знаю');
 	}
 
 	const num = 1;
@@ -161,24 +161,24 @@ console.log("Будущая стоимость инвестиции: $" + FV);
 	const fordFocusPrice = 10000;
 	const budget = 2007000;
 
-let message = '123';
+	let message = '123';
 
-if (budget > 100000) 
-	message = "BMW";
-else if (budget > fordFocusPrice) {
-	message = 'Ford';
-}else {
-	message = 'Байк';
-}
+	if (budget > 100000)
+		message = "BMW";
+	else if (budget > fordFocusPrice) {
+		message = 'Ford';
+	} else {
+		message = 'Байк';
+	}
 
-console.log('Я хочу купити');
+	console.log('Я хочу купити');
 	console.log(`Я хочу купити ${message}`);
 
-budget > 100000 ? console.log('BMW') : console.log('Ford');
+	budget > 100000 ? console.log('BMW') : console.log('Ford');
 
-const str = budget > 100000 ? 'BMW' : 'Ford';
-console.log(str);
-console.log(`Я можу придбати + ${budget > BMVX3Price ? 'BMW,' : 'Ford,'}`);
+	const str = budget > 100000 ? 'BMW' : 'Ford';
+	console.log(str);
+	console.log(`Я можу придбати + ${budget > BMVX3Price ? 'BMW,' : 'Ford,'}`);
 
 
 
@@ -186,5 +186,18 @@ console.log(`Я можу придбати + ${budget > BMVX3Price ? 'BMW,' : 'Fo
 
 // Chapter 5 Lesson 25 Task
 {
-	const question = prompt `Яка буде відповідь`
+	const question = prompt`Яка буде відповідь`
+}
+
+// Chapter 5 Lesson 27 Оператори логіки
+{
+	const isAdmin = false;
+	const canWrite = true;
+	console.log(`Системний файл ${isAdmin && canWrite}`);
+	console.log(`Звичайний файл ${isAdmin || canWrite}`);
+}
+
+// Chapter 5 Lesson 30 Task
+{
+	
 }
